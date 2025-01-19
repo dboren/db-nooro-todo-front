e58554e47c412ca3da749d585a9c1c02f33cefc5
Daniel Boren <danielboren52@gmail.com>
2025-01-19 17:22:15 -0600

    added button directly to homepage instead of as component

--- nooro-todo-front/src/app/page.tsx
+++ nooro-todo-front/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <h1>TODO App</h1>
-          <h2>Placeholder for incoming create task button</h2>
+          <button>Create Task</button>
           <p>Placeholder for total tasks counter</p>
           <p>Placeholder for completed task counter</p>
           <p>Create tasks and organize your to-do items</p>
