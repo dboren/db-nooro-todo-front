8161201bf062e89fe6d8cd05561e86eba0d58aef
Daniel Boren <danielboren52@gmail.com>
2025-01-20 21:57:37 -0600

    reorganized file structure, added global header, and placeholder page files for new-task-form and edit-task-form

--- nooro-todo-front/src/app/layout.tsx
+++ nooro-todo-front/src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <h1 className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center font-[family-name:var(--font-geist-sans)]">Todo App</h1>
         {children}
       </body>
     </html>
