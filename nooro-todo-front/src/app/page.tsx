cd4b12db33b904e3a9c8f81897b9ab4b7d3f3ae3
Daniel Boren <danielboren52@gmail.com>
2025-01-21 17:55:57 -0600

    added axios call to database. Confirmed it is retrieving existing tasks from database

--- nooro-todo-front/src/app/page.tsx
+++ nooro-todo-front/src/app/page.tsx
@@ -1,6 +1,21 @@
+'use client'
+
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+// import TaskCard from './ui/TaskCard';
 
 export default function Home() {
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    // Fetch tasks from back-end
+    axios.get('http://localhost:8080/api/v1/task/getall').then((res) => setTasks(res.data));
+  }, []);
+
+  console.log(JSON.stringify(tasks));
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <Link href="/task-form/new-task-form">
--- nooro-todo-front/src/app/ui/TaskCard.tsx
+++ nooro-todo-front/src/app/ui/TaskCard.tsx
@@ -1,26 +1,22 @@
 export function Card({
     title,
-    color,
+    // color,
     completed,
   }: {
     title: string;
     valcolor: string;
     completed: boolean;
   }) {
-    const Icon = iconMap[type];
-  
+    
+  //Not sure how color is meant to be implemented here based on the figma images provided
+
     return (
-      <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+      <div className="rounded-xl bg-gray-50 p-2">
         <div className="flex p-4">
-          {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-          <h3 className="ml-2 text-sm font-medium">{title}</h3>
+            {/* checkbubble */}
+          <h3 className="text-sm font-medium">{title}</h3>
+          {/* delete button */}
         </div>
-        <p
-          className={`${lusitana.className}
-            truncate rounded-xl bg-white px-4 py-8 text-center text-2xl`}
-        >
-          {value}
-        </p>
       </div>
     );
   }
